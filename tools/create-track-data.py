1b8db0d46aea52f3c75adf32a6841356d42ff268
David Means <75943993+W1T3H4T@users.noreply.github.com>
2024-09-08 11:01:12 -0400

    Refactored
    Used 'create-find-pan' as the source, refactored for track data.

--- tools/create-track-data.py
+++ tools/create-track-data.py
@@ -27,44 +27,132 @@
 #  SOFTWARE.
 #  ===========================================================================
 
-import random
-import re
 import argparse
+from datetime import datetime
+import names
+import random
+from random_address import real_random_address
+import re
+import string
 
-def generate_track_1_data():
-    stop = False
-    while not stop:
-        card_number = random.randint(10**12, 10**19)
-        name = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ ') for _ in range(random.randint(1, 26)))
-        discretionary_data = random.randint(10**1, 10**19)
-        track_1_data = f'%B{card_number}^{name}^{discretionary_data}?'
-        if re.match(r'%B\d{13,19}\^\w{1,26}\^\d{1,19}|\d{1,19}\?', track_1_data):
-            stop = True
-    return track_1_data
+##  =======================================================
+##  Calculate LUHN Checksum
+##  =======================================================
+def luhn_checksum(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d * 2))
+    return checksum % 10
 
-def generate_track_2_data():
-    stop = False
-    while not stop:
-        card_number = random.randint(10**12, 10**19)
-        discretionary_data = random.randint(10**1, 10**19)
-        track_2_data = f';{card_number}={discretionary_data}?'
-        if re.match(r';\d{13,19}=\d{1,19}|\d{1,19}\?', track_2_data):
-            stop = True
-    return track_2_data
+##  =======================================================
+##  Generate PAN
+##  =======================================================
+def generate_luhn_valid_pan(length):
+    pan="111111"
+    while not ( luhn_checksum(pan) == 0):
+        pan = random.choice(['3', '4', '5', '6'])
+        pan += ''.join(str(random.randint(0, 9)) for _ in range(length - 2))  # Generate a random PAN of length-2
+        checksum_digit = (10 - luhn_checksum(pan + '0')) % 10  # Calculate Luhn checksum
+        pan = pan + str(checksum_digit)  # Append checksum digit to the PAN
+    return pan
 
+##  =======================================================
+##  Generate random name
+##  =======================================================
+def generate_random_name():
+    first_name = names.get_first_name()
+    last_name = names.get_last_name()
+    return f"{last_name}/{first_name}"
+
+##  =======================================================
+##  Generate Random Address
+##  =======================================================
+def generate_random_address():
+    try:
+        data=real_random_address()
+        # return f"{data['address1']} {data['city']} {data['state']} {data['postalCode']}"
+        return f"{data['postalCode']}"
+    except Exception:
+        pass
+    return "30308"
+
+##  =======================================================
+##  Generate expiration date
+##  =======================================================
+def generate_valid_date():
+    future_year = random.randint(datetime.now().year % 100, (datetime.now().year + 10) % 100)
+    future_month = random.randint(1, 12)
+    return f"{future_year:02d}{future_month:02d}"
+
+##  =======================================================
+##  Generate service code
+##  =======================================================
+def generate_service_code():
+    digit1 = random.choice(['1', '2', '5', '6'])  # Use typical values
+    digit2 = random.choice(['0', '2', '4'])  # For card-present options
+    digit3 = random.choice(['0', '1', '2', '3', '5', '6', '7'])  # PIN and other restrictions
+    return f"{digit1}{digit2}{digit3}"
+
+##  =======================================================
+##  Generate random discretionary data
+##  =======================================================
+def generate_discretionary_data(length=10):
+    return ''.join(random.choices(string.digits, k=length))
+
+##  =======================================================
+##  Get a random format code of B or M
+##  =======================================================
+def generate_format_code():
+    number = random.randint(0, 99)  # Generate a random number between 0 and 99
+    return 'B' if number >= 50 else 'M'
+
+##  =======================================================
+##  Generate a full Track 1 data
+##  =======================================================
+def generate_track1_data():
+    pan_lengths = [13, 15, 16, 19]
+    pan = generate_luhn_valid_pan(random.choice(pan_lengths))
+    name = generate_random_name()
+    address = generate_random_address()
+    expiration_date = generate_valid_date()
+    service_code = generate_service_code()
+    discretionary_data = generate_discretionary_data()
+    name_addr=f"{name},{address}"
+    # Build Track 1 data
+    track1_data = f"%{generate_format_code()}{pan}^{name_addr[:26]}^{expiration_date}{service_code}{discretionary_data}?"
+    return track1_data
+
+##  =======================================================
+##  Generate a full Track 2 data
+##  =======================================================
+def generate_track2_data():
+    pan_lengths = [13, 15, 16, 19]
+    pan = generate_luhn_valid_pan(random.choice(pan_lengths))
+    name = generate_random_name()
+    expiration_date = generate_valid_date()
+    service_code = generate_service_code()
+    discretionary_data = generate_discretionary_data()
+
+    # Build Track 1 data
+    track2_data = f";{pan}={expiration_date}{service_code}{discretionary_data}?"
+    return track2_data
+
+
+##  =======================================================
+##  MAIN
+##  =======================================================
 def main():
     parser = argparse.ArgumentParser(description="Generate random track data based on provided patterns.")
-    parser.add_argument("--count", type=int, default=100, help="Number of patterns to create for each track type")
+    parser.add_argument("--count", type=int, default=200, help="Number of patterns to create for each track type")
     args = parser.parse_args()
 
-    # Specify the track patterns
-    track_patterns = [
-        r'%B\d{13,19}\^\w{1,26}\^\d{1,19}|\d{1,19}\?',  # Track 1 Data
-        r';\d{13,19}=\d{1,19}|\d{1,19}\?'               # Track 2 Data
-    ]
-
     # Generate and print N track data for each track pattern
-    track_generators = [generate_track_1_data, generate_track_2_data]
+    track_generators = [generate_track1_data, generate_track2_data]
     for generator in track_generators:
         print(f"##\n## Generated data for {generator.__name__}\n##")
         for _ in range(args.count):
