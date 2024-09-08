49795980a96d4df580a6bd3b09a373099b8b7e83
David Means <75943993+W1T3H4T@users.noreply.github.com>
2024-09-08 10:55:15 -0400

    Normalized grep finders
    Corrected grep finder inconsistencies

--- bin/grep-find-pan.sh
+++ bin/grep-find-pan.sh
@@ -6,8 +6,8 @@
 #   Who:    :   David Means <w1t3h4t@gmail.com>
 #   Notes   :   Hits reported by this tool do not necessarily indicate the 
 #           :   presence of actual PAN data used in a financial 
-#           :   transaction.  Luhn checking is NOT performed, test PAN
-#           :   numbers are not detected.
+#           :   transaction.  Luhn checking is NOT performed, and test
+#           :   PAN data is detected.
 #   ==========================================================================
 #
 #   MIT License
@@ -39,9 +39,10 @@ _LOG_INFO=1
 _LOG_DEBUG=0
 _LOG_WARN=1
 _LOG_ERROR=1
-_LOG_TIMESTAMP=0
+_LOG_TIMESTAMP=1
 _LOG_INIT_MESSAGE=0
-_LOG_FILE=$(pwd)/Logs/grep-find-pan.log
+_LOG_FILE=$(pwd)/$(basename $0 .sh).log
+rm -f ${_LOG_FILE}
 
 ##  --------------------------------------------
 ##  Load our logging functions
@@ -59,10 +60,11 @@ fi
 
 TMPFILE=/tmp/$(basename $0 .sh).tmp
 directory=""
+
 ##  Require a prefix of one of the following before the 'PAN':
 ##  whitespace, bracket, double quote, or brace
 REGEX_PREFIX="[ \t'\[\"{]"
-ENABLE_PREFIX=1
+ENABLE_PREFIX=0
 
 visa_regex='4[0-9]{12}(?:[0-9]{3})?'
 mastercard_regex='(5[1-5]|[23][6-7]|8[0-9]|2[0-1][0-9]|22[1-8])\d{14}'
@@ -70,7 +72,7 @@ amex_regex='3[47][0-9]{13}'
 discover_regex='(6(?:011|5[0-9]{2})|[4-7][0-9])[0-9]{12}'
 diners_regex='3(?:0[0-5]|[68][0-9])[0-9]{11}'
 jcb_regex='(35[0-9]{2})[0-9]{12}'
-PAN_REGEX="(${visa_regex}|${mastercard_regex}|${amex_regex}|${discover_regex}|${diners_regex}|${jcb_regex})"
+PATTERNS="(${visa_regex}|${mastercard_regex}|${amex_regex}|${discover_regex}|${diners_regex}|${jcb_regex})"
 
 ## see also: https://baymard.com/checkout-usability/credit-card-patterns
 
@@ -82,14 +84,21 @@ function help()
     echo 
     echo "--path DIRECTORY  The filesystem to search."
     echo "--help|-help|-h   This information."
-    echo "--noprefix        Do not use regex prefix \"${REGEX_PREFIX}\""
+    echo "--rgx-prefix      Require the REGEX prefix of '$REGEX_PREFIX'"
     echo
 }
 
 function finder()
 {
+    ## Formulate our REGEX patterns using 'ENABLE_PREFIX'
     if [[ $ENABLE_PREFIX -eq 1 ]]; then
-        if command grep -E "${REGEX_PREFIX}${PAN_REGEX}" "$1" > $TMPFILE 2>/dev/null; then
+        REGEX="${REGEX_PREFIX}${PATTERNS}"
+            else
+        REGEX="${PATTERNS}"
+        fi
+    log_info "Processing ${1}"
+
+    if command grep -E "${REGEX}" "$1" > $TMPFILE 2>/dev/null; then
             log_info "$ftype: $1 (matches)"
             if [[ -f $_LOG_FILE ]] ; then
                 # cat $TMPFILE && echo | tee -a $_LOG_FILE
@@ -99,18 +108,7 @@ function finder()
                 echo
             fi
         fi
-    else
-        if command grep -E "${PAN_REGEX}" "$1" > $TMPFILE 2>/dev/null; then
-            log_info "$ftype: $1 (matches)"
-            if [[ -f $_LOG_FILE ]] ; then
-                # cat $TMPFILE && echo | tee -a $_LOG_FILE
-                cat $TMPFILE | tee -a $_LOG_FILE
-            else
-                cat $TMPFILE 
-                echo
-            fi
-        fi
-    fi
+
 }
 
 function findFiles()
@@ -118,22 +116,31 @@ function findFiles()
     find "$1" -type f | while read -r FILE
     do
         ftype=$(file --mime-type "$FILE" | cut -d ':' -f 2 | sed 's/ //g')
+        log_debug "Testing: $ftype: $FILE"
         case "${ftype}" in
             "application/x-mach-binary")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/octet-stream")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/x-git")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/zip")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/x-bzip2")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/gzip")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "image/jpeg")
+                log_info("Skipping Image file: ${FILE}"
                 continue;;
             "image/png")
+                log_info("Skipping Image file: ${FILE}"
                 continue;;
             *) 
                 log_debug "Testing: $ftype: $FILE"
@@ -143,28 +150,34 @@ function findFiles()
     done
 }
 
-case "$1" in 
-    '--help|-help|-h')
-        help
-        exit
-        ;;
 
-    '--path')
-        shift
-        directory=$1
-        ;;
+while [[ $# -gt 0 ]]; do
+    # log_info "arg count: $#"
+    case "$1" in 
+        '--help|-help|-h')
+            help
+            exit
+            ;;
 
-    '--noprefix')
-        ENABLE_PREFIX=0
-        ;;
-    *)
-        help
-        exit
-        ;;
-esac
+        '--path')
+            shift
+            directory=$1
+            shift
+            ;;
+
+       '--rgx-prefix')
+            ENABLE_PREFIX=1
+            shift
+            ;;
+        *)
+            help
+            exit
+            ;;
+    esac
+done
 
 if [[ ! -z "$directory" ]] ; then
-    log_info "Processing ${directory}"
     findFiles "${directory}"
 fi
+
 
--- bin/grep-find-track.sh
+++ bin/grep-find-track.sh
@@ -5,9 +5,9 @@
 #           :   TRACK 1 or TRACK 2 data within files.
 #   Who:    :   David Means <w1t3h4t@gmail.com>
 #   Notes   :   Hits reported by this tool do not necessarily indicate the 
-#           :   presence of actual TRACK or PAN data used in a financial 
-#           :   transaction.  Luhn checking is NOT performed, test PAN
-#           :   numbers are not detected.
+#           :   presence of actual TRACK data used in a financial 
+#           :   transaction.  Luhn checking is NOT performed, and test
+#           :   TRACK data is detected.
 #   ==========================================================================
 #   MIT License
 #
@@ -38,9 +38,10 @@ _LOG_INFO=1
 _LOG_DEBUG=0
 _LOG_WARN=1
 _LOG_ERROR=1
-_LOG_TIMESTAMP=0
+_LOG_TIMESTAMP=1
 _LOG_INIT_MESSAGE=0
-_LOG_FILE=$(pwd)/Logs/grep-find-track.log
+_LOG_FILE=$(pwd)/$(basename $0 .sh).log
+rm -f ${_LOG_FILE}
 
 ##  --------------------------------------------
 ##  Load our logging functions
@@ -56,40 +57,46 @@ else
     exit 1 
 fi
 
-TMPFILE=/tmp/$(basename $0 .sh)-temp-data.tmp
+TMPFILE=/tmp/$(basename $0 .sh).tmp
 directory=""
 
 ##  Require a prefix of one of the following before the track data: 
 ##  whitespace, bracket, double quote, or brace
 REGEX_PREFIX="[ \t'\[\"{]"
-ENABLE_PREFIX=1
+ENABLE_PREFIX=0
 
 TRACK1="%[BM][0-9]{12,21}[\^][[:print:]]+[\^][0-9]+[?]"
 TRACK2=";[0-9{21,21}=[0-9]+[?]"
+PATTERNS="${TRACK1}|${TRACK2}"
 
 function help()
 {
     echo 
-    echo "Usage: $(basename $0) --dir DIRECTORY [--skip-zip] [--skip-image]"
+    echo "Usage: $(basename $0) --dir DIRECTORY"
     echo 
     echo "--path DIRECTORY  The filesystem to search."
     echo "--help|-help|-h   This information."
-    echo "--noprefix        Do not use the REGEX prefix '$REGEX_PREFIX'"
+    echo "--rgx-prefix      Require the REGEX prefix of '$REGEX_PREFIX'"
     echo
 }
 
 function finder()
 {
+    ## Formulate our REGEX patterns using 'ENABLE_PREFIX'
     if [[ $ENABLE_PREFIX -eq 1 ]]; then
-        if command grep -E "${REGEX_PREFIX}($TRACK1|$TRACK2)" "$1" > $TMPFILE 2>/dev/null; then
-            log_info "$ftype: $1 (matches)"
-            cat $TMPFILE
-            echo
-        fi
+        REGEX="${REGEX_PREFIX}${PATTERNS}"
     else
-        if command grep -E "($TRACK1|$TRACK2)" "$1" > $TMPFILE 2>/dev/null; then
-            log_info "$ftype: $1 (matches)"
-            cat $TMPFILE
+        REGEX="${PATTERNS}"
+    fi
+    log_info "Processing ${1}"
+
+    if command grep -E "${REGEX}" "$1" > $TMPFILE 2>/dev/null; then
+        log_info "$ftype: $1 (matches)"
+        if [[ -f $_LOG_FILE ]] ; then
+            # cat $TMPFILE && echo | tee -a $_LOG_FILE
+            cat $TMPFILE | tee -a $_LOG_FILE
+        else
+            cat $TMPFILE 
             echo
         fi
     fi
@@ -104,51 +111,64 @@ function findFiles()
         log_debug "Testing: $ftype: $FILE"
         case "${ftype}" in
             "application/x-mach-binary")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/octet-stream")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/x-git")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/zip")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/x-bzip2")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "application/gzip")
+                log_info("Skipping binary file: ${FILE}"
                 continue;;
             "image/jpeg")
+                log_info("Skipping Image file: ${FILE}"
                 continue;;
             "image/png")
+                log_info("Skipping Image file: ${FILE}"
                 continue;;
             *) 
+                log_debug "Testing: $ftype: $FILE"
                 finder "$FILE"
                 ;;
         esac
     done
 }
 
-case "$1" in 
-    '--help|-help|-h')
-        help
-        exit
-        ;;
 
-    '--path')
-        shift
-        directory=$1
-        ;;
+while [[ $# -gt 0 ]]; do
+    # log_info "arg count: $#"
+    case "$1" in 
+        '--help|-help|-h')
+            help
+            exit
+            ;;
 
-    '--noprefix')
-        ENABLE_PREFIX=0
-        ;;
+        '--path')
+            shift
+            directory=$1
+            shift
+            ;;
 
-    *)
-        help
-        exit
-        ;;
-esac
+        '--rgx-prefix')
+            ENABLE_PREFIX=1
+            shift
+            ;;
+        *)
+            help
+            exit
+            ;;
+    esac
+done
 
 if [[ ! -z "$directory" ]] ; then
-    log_info "Processing ${directory}"
     findFiles "${directory}"
 fi
 
