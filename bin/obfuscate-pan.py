796f530331b36a9761cf4e88e69a3ade97bd09e8
David Means <75943993+W1T3H4T@users.noreply.github.com>
2024-09-15 19:16:41 -0400

    Moved scripts in tools into bin

--- bin/obfuscate-pan.py
+++ bin/obfuscate-pan.py
@@ -29,7 +29,13 @@
 import sys
 import re
 
-# Function to replace the middle digits with '*' and add two hyphens evenly spaced
+unique_numbers = set()                  #   Store unique obfuscated numbers
+card_pattern = r"(\d{6})(\d+)(\d{4}$)"  #   Regex for card pattern
+
+#   =================================================================
+#   Function to replace the middle digits with '*' and add two 
+#   hyphens evenly spaced
+#   =================================================================
 def replace_with_asterisks_and_hyphens(match):
     first_group, second_group, third_group = match.groups()
     # Insert two hyphens
@@ -37,27 +43,35 @@ def replace_with_asterisks_and_hyphens(match):
     return f"{first_group}-{hyphens}-{third_group}"
 
 
+#   =================================================================
+#   Obfuscate card numbers and remove the match pattern from the output
+#   =================================================================
 def process_line(input_text):
-    # Obfuscate card numbers and remove the match pattern from the output
     if input_text[0] == ";": return None
     cleaned_text = re.sub(r"^.+match='", "", input_text)
     cleaned_text = re.sub(r"[>'\n\r]", "", cleaned_text)
     return cleaned_text
 
 
-# Store unique obfuscated numbers
-unique_numbers = set()
-card_pattern = r"(\d{6})(\d+)(\d{4}$)"
+#   =================================================================
+#   MAIN: Read from stdin and process each line
+#   =================================================================
+def main():
+    for line in sys.stdin:
+        # Extract numbers and sort uniquely
+        number = process_line(line)
+        if not number is None:
+            unique_numbers.add(number)
 
-# Read from stdin and process each line
-for line in sys.stdin:
-    # Extract numbers and sort uniquely
-    number = process_line(line)
-    if not number is None:
-        unique_numbers.add(number)
+    #   Generate and print the obfuscated PAN
+    for number in unique_numbers:
+        obfuscated_text = re.sub( card_pattern, replace_with_asterisks_and_hyphens, number )
+        print(f"{obfuscated_text}")
 
-for number in unique_numbers:
-    obfuscated_text = re.sub( card_pattern, replace_with_asterisks_and_hyphens, number )
-    print(f"{obfuscated_text}")
+#   =================================================================
+#   Call our main function
+#   =================================================================
+if __name__ == '__main__':
+    main()
 
 
