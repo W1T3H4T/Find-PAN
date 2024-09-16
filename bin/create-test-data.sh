d09c8001c1b225e589692c7d60dad1f81ac36243
David Means <75943993+W1T3H4T@users.noreply.github.com>
2024-09-15 21:00:43 -0400

    Corrected for new script location

--- bin/create-test-data.sh
+++ bin/create-test-data.sh
@@ -12,10 +12,10 @@ function printCount() {
 }
 echo "Building $(( ${max} * 2 )) test PAN & Track data files"
 while [[ $count -lt $max  ]]; do
-    ./tools/create-pan-data.py   --count 50              > ${thisTest}/pan-log-file-${count}.log
-    ./tools/create-pan-data.py   --count 50 --delimited >> ${thisTest}/pan-log-file-${count}.log
-    ./tools/create-track-data.py --count 50              > ${thisTest}/track-log-file-${count}.log
-    ./tools/create-track-data.py --count 50 --delimited >> ${thisTest}/track-log-file-${count}.log
+    create-pan-data.py   --count 50              > ${thisTest}/pan-log-file-${count}.log
+    create-pan-data.py   --count 50 --delimited >> ${thisTest}/pan-log-file-${count}.log
+    create-track-data.py --count 50              > ${thisTest}/track-log-file-${count}.log
+    create-track-data.py --count 50 --delimited >> ${thisTest}/track-log-file-${count}.log
     count=$(( count + 1 ))
     printCount $count
     #echo "${thisTest}/track-log-file-${count}.log: done"
